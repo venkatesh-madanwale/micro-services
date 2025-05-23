0f00bc04cefbbe8258e157298b6501a8632d2723
venkatesh-madanwale <venkateshjmad@gmail.com>
2025-05-23 09:39:20 +0530

    22_05_2025

--- apps/products/src/main.ts
+++ apps/products/src/main.ts
@@ -1,35 +1,41 @@
-import { NestFactory  } from "@nestjs/core";
+import { NestFactory } from "@nestjs/core";
 import { ProductsModule } from "./products.module";
 import { MicroserviceOptions, Transport } from "@nestjs/microservices";
 import { ConfigService } from "@nestjs/config";
 import { Post } from "@nestjs/common";
 
-async function bootstrap(){
+async function bootstrap() {
   //HTTP communication between Postman and product
-    //this is the http layer
-    const app = await NestFactory.create(ProductsModule)
-    // adding ConfigService as middleware for .env configs
-    const config = app.get(ConfigService) 
-    // registering the port no. for product HTTP
-    const port = config.get<number>("PRODUCT_PORT")
-  
+  //this is the http layer
+  const app = await NestFactory.create(ProductsModule)
+
+  //Allow all the origins to connect to everyone
+  app.enableCors({
+    origin: '*',
+    credential: true
+  })
+  // adding ConfigService as middleware for .env configs
+  const config = app.get(ConfigService)
+  // registering the port no. for product HTTP
+  const port = config.get<number>("PRODUCT_PORT")
+
 
   // TCP communication (Interservice) & registering TCP microservices
-    // setting up the port no. for TCP communication for other services to communicate to Product
-    const tcpPort = config.get<number>("PRODUCT_TCP_PORT")
-    // register TCP microservice
-    app.connectMicroservice<MicroserviceOptions>({
-      transport : Transport.TCP,
-      options: {
-        host : config.get<string>("PRODUCT_TCP_HOST"),
-        port : tcpPort
-      }
-    })
-    
-    //Start TCP microservice
-    await app.startAllMicroservices()
-    //Start HTTP server for image, upload, and CRUD ops.
-    await app.listen(port || 3002)
+  // setting up the port no. for TCP communication for other services to communicate to Product
+  const tcpPort = config.get<number>("PRODUCT_TCP_PORT")
+  // register TCP microservice
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: {
+      host: config.get<string>("PRODUCT_TCP_HOST"),
+      port: tcpPort
+    }
+  })
+
+  //Start TCP microservice
+  await app.startAllMicroservices()
+  //Start HTTP server for image, upload, and CRUD ops.
+  await app.listen(port || 3002)
   console.log("Product micro service listening on port 3002")
 
 }
--- apps/user/src/main.ts
+++ apps/user/src/main.ts
@@ -5,6 +5,13 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(UsersModule);
+
+  //Allow all the origins to connect to everyone
+  app.enableCors({
+    origin: '*',
+    credential: true
+  })
+
   const config = app.get(ConfigService)
 
   //HTTP configs
